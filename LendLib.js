bbdb1e6aa5fe285737adc1cd8ec1da1f2cbb1d1b
Jan Milosh <janmilosh@gmail.com>
2014-03-18 23:22:09 -0400

    Adds categories and ability to list them on page

--- LendLib.js
+++ LendLib.js
@@ -1,16 +1,24 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to LendLib.";
-  };
+lists = new Meteor.Collection("Lists");
 
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
+if (Meteor.isClient) {
+  // Template.hello.greeting = function () {
+  //   return "my list.";
+  // };
+
+  // Template.hello.events({
+  //   'click input': function () {
+  //     // template data, if any, is available in 'this'
+  //     if (typeof console !== 'undefined')
+  //       console.log("You pressed the button");
+  //   }
+  // });
+
+  Template.categories.lists = function() {
+    return lists.find({}, {sort: {Category: 1}});
+  };
 }
+
+
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
